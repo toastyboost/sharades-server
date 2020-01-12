7b0d2813ece2569a516081b0a1f45cc7676751dd
Dmitriy Sergeevich <dmitriy.dubotovkin@gmail.com>
2020-01-12 22:06:19 +0300

    update config

--- src/app.js
+++ src/app.js
@@ -4,9 +4,38 @@ const http = require("https");
 const app = express();
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT ? process.env.PORT : 8000;
 
-require("./socket")(server);
+const io = require("socket.io")(server);
+
+const { addUser, deleteUser } = require("./model/users");
+const { addMessage } = require("./model/chat");
+
+io.on("connection", socket => {
+  const socketID = socket.id;
+  console.log(`socket.id`, socket.id);
+
+  const eventEmiter = require("./lib/eventEmiter.js")(socket);
+
+  socket.on("INIT_SESSION", user => {
+    addUser({ id: socketID, name: user.name });
+    eventEmiter.emitOnline();
+  });
+
+  socket.on("USER_MESSAGE", msg => {
+    addMessage(msg);
+    io.emit("USER_MESSAGE", msg);
+  });
+
+  socket.on("USER_TYPING", () => {
+    io.emit("USER_TYPING", socketID);
+  });
+
+  socket.on("disconnect", () => {
+    deleteUser(socketID);
+    eventEmiter.emitOnline();
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`APP started at ${PORT}`);
