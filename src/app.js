736b29277e56199d28613f439c98e11b646424f4
Dmitriy Sergeevich <dmitriy.dubotovkin@gmail.com>
2020-01-12 23:15:59 +0300

    test

--- src/app.js
+++ src/app.js
@@ -11,17 +11,17 @@ const port = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// setup express
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.text({ type: "text/plain" }));
-app.use(cors());
+// // setup express
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.text({ type: "text/plain" }));
+// app.use(cors());
 
-// setup socket.io
-app.use(function(req, res, next) {
-  res.io = io;
-  next();
-});
+// // setup socket.io
+// app.use(function(req, res, next) {
+//   res.io = io;
+//   next();
+// });
 
 const { addUser, deleteUser } = require("./model/users");
 const { addMessage } = require("./model/chat");
